7e2644aa827008d08ea6130571e1f0820bfc7e87
angrycompany16 <dpersson16@gmail.com>
2025-01-22 11:59:02 +0100

    Timer implemented

---
+++ elev_al_go/timer.go
@@ -0,0 +1,30 @@
+package elevalgo
+
+import "time"
+
+type Timer struct {
+	startTime time.Time
+	timeOut   time.Duration
+	active    bool
+}
+
+func (t *Timer) Start() {
+	t.startTime = time.Now()
+	t.active = true
+}
+
+func (t *Timer) Stop() {
+	t.active = false
+}
+
+func (t *Timer) TimedOut() bool {
+	return t.active && time.Since(t.startTime) > t.timeOut
+}
+
+func MakeTimer() *Timer {
+	return &Timer{
+		time.Now(),
+		time.Second * 3,
+		false,
+	}
+}
