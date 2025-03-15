eacb487bb75be8ebc3822361d2b4977cf96867eb
angrycompany16 <dpersson16@gmail.com>
2025-03-15 16:47:06 +0100

    Cleanup and gitignore

--- types/types.go
+++
@@ -1,8 +0,0 @@
-package types
-
-// type Heartbeat struct {
-// 	SenderId        string
-// 	State           elevalgo.Elevator
-// 	PendingRequests p2p.PendingRequestList // Ack list
-// 	WorldView       map[string]peer.Peer   // Our worldview
-// }
